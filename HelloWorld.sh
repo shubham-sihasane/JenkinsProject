877add12d09557137128839bcef703122126abd4
Shubham Sihasane <78297361+shubham-sihasane@users.noreply.github.com>
2021-07-25 20:17:30 +0530

    Initial Commit

---
+++ HelloWorld.sh
@@ -0,0 +1,15 @@
+############################################################################
+
+# Author        : Shubham Sihasane
+# Description   : Write a simple bash program to print a message on the console screen.
+
+############################################################################
+
+
+# Print a text on sinle line
+echo "Hello World!"
+# Print a text on multiple lines.
+echo "Welcome to the World of DevOps"
+
+
+##############################END OF THE SCRIPT##############################
